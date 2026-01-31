961122bf0015b502f5daabed4e8fe3c877b02830
rapz-cloud <ayuulestarii0391@gmail.com>
2026-01-31 07:18:41 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8003564302:AAHXuoVjeqUI-9uxZzvDoPVVAVQ_vYMjXDM",  // Ubah Jadi Token Bot Mu !!!
+  owner: "8136310152", // Ubah Jadi Id Mu !!!
+  port: "3005", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.162.115" // Ubah Jadi Ip Vps Mu !!!
+};
